181b3037f514922a2ec1488524e13f0a541216d2
kakashiio <john.cha@qq.com>
2024-10-15 23:48:21 +0800

    Basic

---
+++ Runtime/BasePool.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace IO.Unity3D.Source.Pool
+{
+    //******************************************
+    //  
+    //
+    // @Author: Kakashi
+    // @Email: john.cha@qq.com
+    // @Date: 2024-10-15 22:17
+    //******************************************
+    public class BasePool<T> : IPool<T>
+    {
+        protected Stack<T> _Cache = new Stack<T>();
+
+        private readonly int _InitSize;
+        private readonly int _MaxSize;
+        private readonly Func<Task<T>> _Creator;
+        private readonly Action<T> _OnReturn;
+        private readonly Action<T> _OnDestroy;
+        private readonly Action<T> _OnBorrow;
+
+        private bool _Destroyed;
+        
+        public static async Task<BasePool<T>> Build(int initSize, int maxSize, Func<Task<T>> creator, Action<T> onBorrow, Action<T> onReturn, Action<T> onDestroy)
+        {
+            var pool = new BasePool<T>(initSize, maxSize, creator, onBorrow, onReturn, onDestroy);
+            await pool.Init();
+            return pool;
+        }
+
+        protected BasePool(int initSize, int maxSize, Func<Task<T>> creator, Action<T> onBorrow, Action<T> onReturn, Action<T> onDestroy)
+        {
+            _MaxSize = Mathf.Max(maxSize, initSize);
+            _InitSize = initSize;
+            _Creator = creator;
+            _OnBorrow = onBorrow ?? _DoNothing;
+            _OnReturn = onReturn ?? _DoNothing;
+            _OnDestroy = onDestroy ?? _DoNothing;
+        }
+
+        private void _DoNothing(T obj)
+        {
+        }
+
+        protected async Task Init()
+        {
+            for (int i = 0; i < _InitSize; i++)
+            {
+                var t = await _Creator();
+                Return(t);
+            }
+        }
+
+        public async Task<T> Borrow()
+        {
+            if (_Destroyed)
+            {
+                Debug.LogError("Can not borrow from a destroyed pool");
+                return default(T);
+            }
+
+            if (_Cache.TryPop(out T t))
+            {
+                _OnBorrow(t);
+                return t;
+            }
+
+            t = await _Creator();
+            _OnBorrow(t);
+            return t;
+        }
+
+        public void Return(T t)
+        {
+            if (_Cache.Count < _MaxSize && !_Destroyed)
+            {
+                _OnReturn(t);
+                _Cache.Push(t);
+            }
+            else
+            {
+                _OnDestroy(t);
+            }
+        }
+
+        public void Destroy()
+        {
+            _Destroyed = true;
+            foreach (var t in _Cache)
+            {
+                _OnDestroy(t);
+            }
+            _Cache.Clear();
+        }
+    }
+}
---
+++ Samples~/Basic/Scene/BasicDemo.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace IO.Unity3D.Source.Pool.Sample
+{
+    public class BasicDemo : MonoBehaviour
+    {
+
+        public GameObject Prefab;
+        
+        private DebugPool<A> _CSharpObjectPool;
+        private DebugPool<GameObject> _GameObjectObjectPool;
+
+        private Stack<A> _Stack1 = new Stack<A>();
+        private Stack<GameObject> _Stack2 = new Stack<GameObject>();
+        
+        async void Start()
+        {
+            _CSharpObjectPool = await DebugPool<A>.Build(1, 2, async () => new A(), a => Debug.Log("Borrow " + a), a => Debug.Log("Return " + a), a => Debug.Log("Destroy " + a));
+
+
+            int id = 0;
+            _GameObjectObjectPool = await DebugPool<GameObject>.Build(1, 2, async () =>
+            {
+                var asyncInstantiateOperation = InstantiateAsync(Prefab);
+
+                var tcs = new TaskCompletionSource<GameObject>();
+
+                StartCoroutine(_InstantiateAsync(asyncInstantiateOperation, tcs));
+                    
+                var go = await tcs.Task;
+                go.name = id++.ToString();
+                go.transform.position = Random.insideUnitSphere;
+                return go;
+            }, a =>
+            {
+                a.SetActive(true);
+                Debug.Log("Borrow " + a);
+            }, a =>
+            {
+                a.SetActive(false);
+                Debug.Log("Return " + a);
+            }, a =>
+            {
+                Debug.Log("Destroy " + a);
+                GameObject.Destroy(a);
+            });
+            Debug.Log(_GameObjectObjectPool);
+        }
+
+        private IEnumerator _InstantiateAsync(AsyncInstantiateOperation<GameObject> asyncInstantiateOperation, TaskCompletionSource<GameObject> tcs)
+        {
+            yield return asyncInstantiateOperation;
+            tcs.SetResult(asyncInstantiateOperation.Result[0]);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Pool");
+            foreach (var a in _CSharpObjectPool.Cache)
+            {
+                GUILayout.Button(a.ToString());
+            }
+            GUILayout.EndHorizontal();
+            
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Using");
+            foreach (var a in _Stack1)
+            {
+                GUILayout.Button(a.ToString());
+            }
+            GUILayout.EndHorizontal();
+            
+            
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("GameObject Pool");
+            foreach (var a in _GameObjectObjectPool.Cache)
+            {
+                GUILayout.Button(a.ToString());
+            }
+            GUILayout.EndHorizontal();
+            
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Using");
+            foreach (var a in _Stack2)
+            {
+                GUILayout.Button(a.ToString());
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        async void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                var a = await _CSharpObjectPool.Borrow();
+                if (a != null)
+                {
+                    _Stack1.Push(a);    
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                if (_Stack1.TryPop(out A a))
+                {
+                    _CSharpObjectPool.Return(a);
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                _Stack1.Clear();
+                _CSharpObjectPool.Destroy();
+            }
+            
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                var a = await _GameObjectObjectPool.Borrow();
+                if (a != null)
+                {
+                    _Stack2.Push(a);    
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                if (_Stack2.TryPop(out GameObject a))
+                {
+                    _GameObjectObjectPool.Return(a);
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                foreach (var go in _Stack2)
+                {
+                    GameObject.Destroy(go);
+                }
+                _Stack2.Clear();
+                _GameObjectObjectPool.Destroy();
+            }
+        }
+    }
+
+    class DebugPool<T> : BasePool<T>
+    {
+        public IEnumerable<T> Cache => _Cache;
+        
+        public static async Task<DebugPool<T>> Build(int initSize, int maxSize, Func<Task<T>> creator, Action<T> onBorrow, Action<T> onReturn, Action<T> onDestroy)
+        {
+            var pool = new DebugPool<T>(initSize, maxSize, creator, onBorrow, onReturn, onDestroy);
+            await pool.Init();
+            return pool;
+        }
+
+        protected DebugPool(int initSize, int maxSize, Func<Task<T>> creator, Action<T> onBorrow, Action<T> onReturn, Action<T> onDestroy) : base(initSize, maxSize, creator, onBorrow, onReturn, onDestroy)
+        {
+        }
+    }
+
+    class A
+    {
+        private static int _IDG = 0;
+        
+        private int _ID = _IDG++;
+
+        public override string ToString()
+        {
+            return $"{_ID}";
+        }
+    }
+
+}
